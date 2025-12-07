e89a7cb4e318126a6c322af0d38cb43cb9c77c3f
Terence-1 <100476796+Terence-1@users.noreply.github.com>
2025-12-07 22:08:10 +0000

    update v1 qmk

--- myKeyboardV1/qmk/0terence/keymaps/vial/keymap.c
+++ myKeyboardV1/qmk/0terence/keymaps/vial/keymap.c
@@ -4,6 +4,7 @@ enum {
     TD_Q_ESC,
 };
 
+__attribute__((weak))
 tap_dance_action_t tap_dance_actions[] = {
     [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC)  
 };
@@ -16,8 +17,9 @@ enum combos {
 
 const uint16_t PROGMEM spctab_combo[] = {LT(1,KC_SPC), LT(2,KC_TAB), COMBO_END};
 const uint16_t PROGMEM entbspc_combo[] = {LT(3,KC_ENT), LT(4, KC_BSPC), COMBO_END};
-const uint16_t PROGREM fjcaps_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
+const uint16_t PROGMEM fjcaps_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 
+__attribute__((weak))
 combo_t key_combos[] = {
     [SPCTAB_LOCK] = COMBO(spctab_combo, QK_LLCK),
     [ENTBSPC_LOCK] = COMBO(entbspc_combo, QK_LLCK),
@@ -44,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______,     _______, _______
     ),
     [3] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,     KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,     _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,
         KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,     _______, _______, _______, _______, _______,
-        KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
                                    _______, _______,     _______, _______
     ),
     [4] = LAYOUT(
-        KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,     KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, _______,
-        _______, _______, _______, _______, _______,     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
+        KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,     _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, 
+        _______, _______, _______, _______, _______,     _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,
         _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______,
                                    _______, _______,     _______, _______
     )
